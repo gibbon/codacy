a035a3bb0139d551642c80df54f7d797ab5ff6c9
Tom Day <tom.day@pexa.com.au>
2021-01-11 15:14:24 +1100

    test

--- test.java
+++ test.java
@@ -1,1 +1,36 @@
-test
+package codacy;
+
+import com.google.common.collect.Sets;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.passay.dictionary.WordLists;
+import org.passay.dictionary.sort.ArraysSort;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.NoResultException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Properties;
+import java.util.Set;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+
+
+@RunWith(PowerMockRunner.class)
+@PowerMockIgnore({"javax.management.*","javax.security.*","javax.script.*"})
+@PrepareForTest({WordLists.class})
+public class PasswordAppServiceImplTest {  
+}
